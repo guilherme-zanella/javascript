15d7865a7ba8b29b292232a5216387ed3a72ea64
Guilherme Zanella <zanellag722@gmail.com>
2026-08-15 21:35:07 -0300

    Exercícios de Condições simples feitos

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+let velocidade = 15
+
+console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+
+if (velocidade > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,10 @@
+const pais = 'Brasil'
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Você é brasileiro')
+}
+else {
+    console.log('Você é estangeiro')
+}
