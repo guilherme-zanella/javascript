77d391589f5fee0f75203c893afe961a246b568d
Guilherme Zanella <zanellag722@gmail.com>
2026-08-20 20:49:42 -0300

    Problema de caixa vazia resolvido

--- aula14-ex/ex016-contador/sozinho/script.js
+++ aula14-ex/ex016-contador/sozinho/script.js
@@ -6,15 +6,20 @@ function contador() {
     const final = Number(document.getElementById('final').value)
     let passo = Number(document.getElementById('passo').value)
 
-    if (passo == 0) {
+    if (inicio == '' || final == '') {
+        alert('Por favor Preencha todos os campos.')
+    }
+    else {
+        if (passo == 0) {
         passo = 1
         alert('Passo inválido! Considerando PASSO = 1')
-    }
+        }
 
-    resultado.innerHTML = ''
-    texto.innerHTML = 'Contando:'
-    for (let n = inicio; n <= final; n += passo) {
-        resultado.innerHTML += `${n} 👉 `
+        resultado.innerHTML = ''
+        texto.innerHTML = 'Contando:'
+        for (let n = inicio; n <= final; n += passo) {
+            resultado.innerHTML += `${n} 👉 `
+        }
+        resultado.innerHTML += '🏁'
     }
-    resultado.innerHTML += '🏁'
 }
