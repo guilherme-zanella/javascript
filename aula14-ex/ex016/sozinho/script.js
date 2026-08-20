733ad89de7a9cffcf57862053d5fdbf8f63aaa6c
Guilherme Zanella <zanellag722@gmail.com>
2026-08-20 19:42:19 -0300

    Estrutura criada e exercício contador feito sozinho finalizado

---
+++ aula14-ex/ex016/sozinho/script.js
@@ -0,0 +1,17 @@
+const texto = document.getElementById('texto')
+const resultado = document.getElementById('resultado')
+
+function contador() {
+    const inicio = Number(document.getElementById('inicio').value)
+    const final = Number(document.getElementById('final').value)
+    const passo = Number(document.getElementById('passo').value)
+    if (passo == 0) {
+        passo = 1
+    }
+    resultado.innerHTML = ''
+    texto.innerHTML = 'Contando:'
+    for (let n = inicio; n <= final; n += passo) {
+        resultado.innerHTML += `${n} 👉 `
+    }
+    resultado.innerHTML += '🏁'
+}
