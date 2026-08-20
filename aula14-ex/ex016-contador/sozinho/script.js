29817f2026d105d281d708632726ae68090feaa9
Guilherme Zanella <zanellag722@gmail.com>
2026-08-20 20:25:19 -0300

    Alguns problemas corrigidos menos o de caixa vazia

--- aula14-ex/ex016-contador/sozinho/script.js
+++ aula14-ex/ex016-contador/sozinho/script.js
@@ -4,10 +4,13 @@ const resultado = document.getElementById('resultado')
 function contador() {
     const inicio = Number(document.getElementById('inicio').value)
     const final = Number(document.getElementById('final').value)
-    const passo = Number(document.getElementById('passo').value)
+    let passo = Number(document.getElementById('passo').value)
+
     if (passo == 0) {
         passo = 1
+        alert('Passo inválido! Considerando PASSO = 1')
     }
+
     resultado.innerHTML = ''
     texto.innerHTML = 'Contando:'
     for (let n = inicio; n <= final; n += passo) {
